8aab787ee6877d14e4ad4f56b3bef7e41b3da067
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-19 21:51:47 +0530

    Add Driver Class 'EmployeeSystem' and add Implementation of Derived classes

---
+++ src/com/practice/dsa/oops/employee/EmployeeSystem.java
@@ -0,0 +1,24 @@
+package com.practice.dsa.oops.employee;
+
+public class EmployeeSystem {
+    public static void main(String[] args) {
+        Employee fullTimeEmployees = new FullTimeEmployees("Vishwajeet","0001",2000000);
+        Employee partTimeEmployees = new PartTimeEmployee("Anil","0010",200, 50);
+        Employee contractor = new Contractor("Shambu","0100",2000000);
+
+        System.out.println("Full Time Employees Details: ");
+        fullTimeEmployees.display();
+        fullTimeEmployees.calculatePay();
+        System.out.println();
+
+        System.out.println("Part Time Employees Details: ");
+        partTimeEmployees.display();
+        partTimeEmployees.calculatePay();
+        System.out.println();
+
+        System.out.println("Contract Type Employees Details: ");
+        contractor.display();
+        contractor.calculatePay();
+        System.out.println();
+    }
+}
