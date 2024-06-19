b563adc6f2adc31950cf2e63261f408d49eb0c4e
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-19 21:23:10 +0530

    Add Derived Class 'PartTimeEmployee' and overide calculate method

---
+++ src/com/practice/dsa/oops/employee/PartTimeEmployee.java
@@ -0,0 +1,18 @@
+package com.practice.dsa.oops.employee;
+
+public class PartTimeEmployee extends Employee {
+    private double hourlyRate;
+    private int hoursWorked;
+
+    public PartTimeEmployee(String name, String employeeId, double hourlyRate, int hoursWorked) {
+        super(name, employeeId);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public void calculatePay() {
+        double payment = hourlyRate * hoursWorked;
+        System.out.println("The the amount to Pay is: "+payment+" for the "+hoursWorked+" Hours worked");
+    }
+}
