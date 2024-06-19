5f03df1870591f8db7cba132420e0bc13c36df5a
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-19 08:01:48 +0530

    Add inheristance example Vehicle

---
+++ src/com/practice/dsa/oops/vehicle/Vehicle.java
@@ -0,0 +1,17 @@
+package com.practice.dsa.oops.vehicle;
+
+public class Vehicle {
+    private String make;
+    private String model;
+    private int year;
+
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    void displayDetails() {
+        System.out.println("Make: "+make+" Model: "+model+" Year: "+year);
+    }
+}
