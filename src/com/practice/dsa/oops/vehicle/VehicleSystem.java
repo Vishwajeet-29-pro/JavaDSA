d7be71f4c99e35913587df706dfc1265111d2643
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-19 08:21:51 +0530

    Add Driver Class 'VehicleSystem' to complete Implementation

---
+++ src/com/practice/dsa/oops/vehicle/VehicleSystem.java
@@ -0,0 +1,24 @@
+package com.practice.dsa.oops.vehicle;
+
+public class VehicleSystem {
+    public static void main(String[] args) {
+        Vehicle car = new Car("Toyota","Innova", 2022,5, false);
+        Vehicle truck = new Truck("TATA","RoadRage",2020,50000, true);
+        Vehicle motorCycle = new MotorCycle("Honda","CB Shine",2011, false);
+
+        System.out.println("Information of the Car:");
+        car.displayDetails();
+        System.out.println();
+
+        System.out.println("Information of the Truck:");
+        truck.displayDetails();
+        System.out.println();
+
+        System.out.println("Information of the Motor Cycle");
+        motorCycle.displayDetails();
+        System.out.println();
+
+    }
+
+
+}
