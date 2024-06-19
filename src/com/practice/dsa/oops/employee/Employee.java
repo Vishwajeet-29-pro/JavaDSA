af53d5a1ca69dd5509b2fd9fe81dc7ab21e35e85
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-19 21:11:28 +0530

    Add Polymorsphism Example with Employee Class

---
+++ src/com/practice/dsa/oops/employee/Employee.java
@@ -0,0 +1,18 @@
+package com.practice.dsa.oops.employee;
+
+abstract class Employee {
+    private String name;
+    private String employeeId;
+
+    public Employee(String name, String employeeId){
+        this.name = name;
+        this.employeeId = employeeId;
+    }
+
+    public abstract void calculatePay();
+
+    public void display(){
+        System.out.println("The Name of the Employee: "+name);
+        System.out.println("The Employee Id is: "+employeeId);
+    }
+}
