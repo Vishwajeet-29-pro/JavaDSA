4e24d96699e0312b187cf0caf92746483305ca64
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-20 09:03:21 +0530

    Add Driver class ShapeDrawingSystem

---
+++ src/com/practice/dsa/oops/shape/ShapeDrawingSystem.java
@@ -0,0 +1,24 @@
+package com.practice.dsa.oops.shape;
+
+public class ShapeDrawingSystem {
+    public static void main(String[] args) {
+        Shape circle = new Circle("Red",5);
+        Shape rectangle = new Rectangle("Blue",10,14.2);
+        Shape triangle = new Triangle("Green",20.23, 2.3);
+
+        System.out.println("Shape Circle: ");
+        circle.calculateArea();
+        circle.displayDetails();
+        System.out.println();
+
+        System.out.println("Shape Rectangle: ");
+        rectangle.calculateArea();
+        rectangle.displayDetails();
+        System.out.println();
+
+        System.out.println("Shape Triangle: ");
+        triangle.calculateArea();
+        triangle.displayDetails();
+        System.out.println();
+    }
+}
