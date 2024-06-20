60bd838678225db29c0dc9cd1814b03c3892400d
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-20 08:52:07 +0530

    Add Derived class 'Rectangle' and override methods

---
+++ src/com/practice/dsa/oops/shape/Rectangle.java
@@ -0,0 +1,22 @@
+package com.practice.dsa.oops.shape;
+
+public class Rectangle extends Shape implements Drawable {
+    private double width;
+    private double height;
+
+    public Rectangle(String color, double width, double height){
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Draw a Rectangle");
+    }
+
+    @Override
+    void calculateArea() {
+        area = width * height;
+    }
+}
