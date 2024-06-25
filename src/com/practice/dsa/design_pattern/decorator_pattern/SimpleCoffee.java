315a7972e717442c1f69edceb46c7022ef8a7f6a
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-25 08:31:42 +0530

    Add Concrete class 'SimpleCoffee' and Implements 'Coffee' interface.

---
+++ src/com/practice/dsa/design_pattern/decorator_pattern/SimpleCoffee.java
@@ -0,0 +1,13 @@
+package com.practice.dsa.design_pattern.decorator_pattern;
+
+public class SimpleCoffee implements Coffee {
+    @Override
+    public String getDescription() {
+        return "Simple Coffee";
+    }
+
+    @Override
+    public double getCost() {
+        return 2.0;
+    }
+}
