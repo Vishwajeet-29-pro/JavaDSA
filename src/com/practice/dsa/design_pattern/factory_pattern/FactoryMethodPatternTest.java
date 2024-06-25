f982746155c5b551a0366ee10d13bbd77c4c5e54
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-25 08:11:48 +0530

    Add description for factory method Pattern

--- src/com/practice/dsa/design_pattern/factory_pattern/FactoryMethodPatternTest.java
+++ src/com/practice/dsa/design_pattern/factory_pattern/FactoryMethodPatternTest.java
@@ -1,5 +1,11 @@
 package com.practice.dsa.design_pattern.factory_pattern;
 
+/*
+* This implementation of the Observer pattern allows multiple investors to be notified whenever there is a change in the
+* stock price, without tightly coupling the stock to the investors. The stock (subject) maintains a list of investors
+* (observers) and notifies them whenever its state changes.
+* */
+
 public class FactoryMethodPatternTest {
     public static void main(String[] args) {
         NotificationFactory notificationFactory = new NotificationFactory();
