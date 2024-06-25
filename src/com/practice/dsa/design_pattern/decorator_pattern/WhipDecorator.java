650f61f36258c98d5f83d801693c971522c3b090
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-25 08:44:20 +0530

    Create Concreate Decorator 'WhipDecorator' class.

---
+++ src/com/practice/dsa/design_pattern/decorator_pattern/WhipDecorator.java
@@ -0,0 +1,17 @@
+package com.practice.dsa.design_pattern.decorator_pattern;
+
+public class WhipDecorator extends CoffeeDecorator {
+    public WhipDecorator(Coffee decoratedCoffee) {
+        super(decoratedCoffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return decoratedCoffee.getDescription() +", whip";
+    }
+
+    @Override
+    public double getCost() {
+        return decoratedCoffee.getCost() + 0.7;
+    }
+}
