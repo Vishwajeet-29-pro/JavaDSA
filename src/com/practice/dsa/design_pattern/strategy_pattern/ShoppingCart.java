e24bbaa1da25ae45e7bcf434777f38bd6a903209
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-26 21:17:10 +0530

    Add Context class 'ShoppingCart'.

---
+++ src/com/practice/dsa/design_pattern/strategy_pattern/ShoppingCart.java
@@ -0,0 +1,13 @@
+package com.practice.dsa.design_pattern.strategy_pattern;
+
+public class ShoppingCart {
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy){
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void checkout(int amount){
+        paymentStrategy.pay(amount);
+    }
+}
