aa3b7c2200def6b4f2e98d8b8df5b02192c855f7
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-27 21:02:03 +0530

    Add Proxy Class: RealImage.

---
+++ src/com/practice/dsa/design_pattern/proxy_pattern/ProxyImage.java
@@ -0,0 +1,18 @@
+package com.practice.dsa.design_pattern.proxy_pattern;
+
+public class ProxyImage implements Image {
+    private RealImage realImage;
+    private String fileName;
+
+    public ProxyImage(String fileName){
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void display() {
+        if (realImage == null){
+            realImage = new RealImage(fileName);
+        }
+        realImage.display();
+    }
+}
