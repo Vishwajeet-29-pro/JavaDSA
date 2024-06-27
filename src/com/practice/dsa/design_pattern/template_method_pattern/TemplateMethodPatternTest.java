7da221a5afabb136a92a0debbd1ec09295a15f77
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-27 21:42:23 +0530

    Add Driver class: TemplateMethodPatternTest.

---
+++ src/com/practice/dsa/design_pattern/template_method_pattern/TemplateMethodPatternTest.java
@@ -0,0 +1,13 @@
+package com.practice.dsa.design_pattern.template_method_pattern;
+
+public class TemplateMethodPatternTest {
+    public static void main(String[] args) {
+        DataProcessor csvDataProcessor = new CSVDataProcessor();
+        csvDataProcessor.processData();
+
+        System.out.println();
+
+        DataProcessor databaseDataProcessor = new DatabaseDataProcessor();
+        databaseDataProcessor.processData();
+    }
+}
