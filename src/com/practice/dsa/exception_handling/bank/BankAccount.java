ee2269731d0104571550d9c4f77b8b77478f2301
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-06-28 08:36:32 +0530

    Add BankAccount class with necessry methods.

---
+++ src/com/practice/dsa/exception_handling/bank/BankAccount.java
@@ -0,0 +1,35 @@
+package com.practice.dsa.exception_handling.bank;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance){
+        if (initialBalance < 0) {
+            throw new IllegalArgumentException("Initial Balance cannot be negative.");
+        }
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) throws InvalidAmountException {
+        if (amount <= 0) {
+            throw new InvalidAmountException("Deposit amount must be positive.");
+        }
+        balance += amount;
+        System.out.println("Deposited. "+amount);
+    }
+
+    public void withdraw(double amount) throws InvalidAmountException, InsufficientFundException {
+        if (amount <= 0) {
+            throw new InvalidAmountException("Withdraw amount must be positive");
+        }
+        if (amount > balance) {
+            throw new InsufficientFundException("Insufficient Fund. Available Balance: "+balance);
+        }
+        balance -= amount;
+        System.out.println("Withdrew: "+amount);
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+}
