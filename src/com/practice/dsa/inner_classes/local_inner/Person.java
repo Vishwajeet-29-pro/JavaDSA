d0d2feda834eeb7dc1d9a1f589ea14c20f19190e
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-07-01 20:49:19 +0530

    Add Person class with some method to implement local inner class.

---
+++ src/com/practice/dsa/inner_classes/local_inner/Person.java
@@ -0,0 +1,28 @@
+package com.practice.dsa.inner_classes.local_inner;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void printPersonDetail() {
+        // Local Inner class
+        class DetailPrinter {
+           public void printDetails() {
+               // Accessing the outer class elements
+               System.out.println("Person Name: "+name);
+               System.out.println("Person Age: "+age);
+           }
+        }
+        DetailPrinter printer = new DetailPrinter();
+        printer.printDetails();
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Vishwajeet",24);
+        person.printPersonDetail();
+    }
+}
