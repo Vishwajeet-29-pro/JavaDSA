c24a2313339e88b8cd012a6f68f401201c694cdb
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-07-03 21:00:29 +0530

    Add failing test for ShoppingCart, create package for ShoppingCart test class.

---
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/problems/shopping/ShoppingCartTest.java
@@ -0,0 +1,19 @@
+package org.practice.dsa.problems.shopping;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShoppingCartTest {
+
+    @Test
+    public void testAddItemToCart() {
+        ShoppingCart cart = new ShoppingCart();
+        Item item = new Item("Apple",20);
+        cart.addItem(item);
+
+        assertEquals(1, cart.getItemCount());
+        assertEquals(20, cart.getTotalprice());
+    }
+}
