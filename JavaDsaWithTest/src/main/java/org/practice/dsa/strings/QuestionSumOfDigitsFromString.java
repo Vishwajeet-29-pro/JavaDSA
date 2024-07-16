95fca7159ab85951e3e364c94c9d60f16cc2eabf
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-07-16 21:21:33 +0530

    Add Logic in the sumOfDigit method and Make test case pass.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/strings/QuestionSumOfDigitsFromString.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/strings/QuestionSumOfDigitsFromString.java
@@ -2,7 +2,13 @@ package org.practice.dsa.strings;
 
 public class QuestionSumOfDigitsFromString {
     public int sumOfDigits(String input) {
-
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (Character.isDigit(ch)){
+                sum += Character.getNumericValue(ch);
+            }
+        }
+        return sum;
     }
 }
