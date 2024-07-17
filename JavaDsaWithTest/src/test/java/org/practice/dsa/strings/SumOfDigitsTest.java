2291fb28c0c5b46dfff2df026db967af7736caa2
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-07-17 08:36:25 +0530

    Refactor: Rename the Test classes.

--- JavaDsaWithTest/src/test/java/org/practice/dsa/strings/SumOfDigitsTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/strings/SumOfDigitsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestSumOfDigits {
+public class SumOfDigitsTest {
 
     @Test
     void testSumOfDigits() {
