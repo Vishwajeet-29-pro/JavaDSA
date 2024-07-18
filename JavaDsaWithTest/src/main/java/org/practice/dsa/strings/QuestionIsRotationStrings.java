58c0726db4111e076577b7ae5bec2eecba17eb1b
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-07-18 08:36:54 +0530

    Add Failing test for rotation String and create class for method implementation.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/strings/QuestionIsRotationStrings.java
@@ -0,0 +1,7 @@
+package org.practice.dsa.strings;
+
+public class QuestionIsRotationStrings {
+    public boolean isRotation(String stringOne, String stringTwo) {
+        return false;
+    }
+}
---
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/strings/RotationStringsTest.java
@@ -0,0 +1,20 @@
+package org.practice.dsa.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RotationStringsTest {
+
+    private QuestionIsRotationStrings rotationStrings = new QuestionIsRotationStrings();
+
+    @Test
+    void testIsRotation() {
+        String stringOne = "waterbottle";
+        String stringTwo = "erbottlewat";
+
+        boolean result = rotationStrings.isRotation(stringOne, stringTwo);
+
+        assertTrue(result);
+    }
+}
