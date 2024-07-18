fb995aec9537227d453d06b4c5cb83144760a4c5
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-07-18 22:33:09 +0530

    Add failing test for Even Number list, implement method.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/functional_programming/QuestionSumOfEvenNumbers.java
@@ -0,0 +1,9 @@
+package org.practice.dsa.java8.functional_programming;
+
+import java.util.List;
+
+public class QuestionSumOfEvenNumbers {
+    public int sumOfEvenNumbers(List<Integer> numberList) {
+        return 0;
+    }
+}
