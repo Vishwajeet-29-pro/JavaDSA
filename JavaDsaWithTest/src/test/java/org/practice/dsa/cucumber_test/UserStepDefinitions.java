1ec94ccd2518b7e893e574c6da63f8fe22458220
Vishwajeet K <vishwajeet.k@vervetronics.com>
2024-07-22 10:43:40 +0530

    Add Step Definitions for user login.

--- JavaDsaWithTest/src/test/java/org/practice/dsa/cucumber_test/UserStepDefinitions.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/cucumber_test/UserStepDefinitions.java
@@ -1,10 +1,29 @@
 package org.practice.dsa.cucumber_test;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.practice.dsa.cucumber_test.some_fake_services.UserService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserStepDefinitions {
+    private UserService userService;
+    private String registrationMessage;
+    private String loginMessage;
+
     @Given("I am on the registration page")
     public void iAmOnTheRegistrationPage() {
         // navigate to the browser
+    }
+
+    @When("I have registered with username {string} and password {string}")
+    public void iHaveRegisteredWithUsernameAndPassword(String username, String password) {
+        registrationMessage = userService.register(username, password);
+    }
+
+    @Then("I should see a success message")
+    public void iShouldSeeASuccessMessage() {
+        assertEquals("Registration Successful", registrationMessage);
     }
 }
