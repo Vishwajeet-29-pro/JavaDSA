7b76fa91b71ae634a4ec4525c34b01e40308ba34
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-07-23 22:32:54 +0530

    Add Test cases for Edge cases

--- JavaDsaWithTest/src/test/java/org/practice/dsa/leet_code_test/RemoveElementsFromSortedArrayTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/leet_code_test/RemoveElementsFromSortedArrayTest.java
@@ -2,6 +2,7 @@ package org.practice.dsa.leet_code_test;
 
 import org.junit.jupiter.api.Test;
 import org.practice.dsa.leet_code.QuestionRemoveElementFromSortedArrays;
+import org.practice.dsa.leet_code.QuestionRemoveElements;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,6 +21,42 @@ public class RemoveElementsFromSortedArrayTest {
     public void testRemoveElementsFromSortedArray() {
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
         int expectedK = 5;
+
+        int result = QuestionRemoveElementFromSortedArrays.removeDuplicates(nums);
+        assertEquals(expectedK, result);
+    }
+
+    @Test
+    public void testNoDuplicates() {
+        int[] nums = {1,2,3,4,5};
+        int expectedK = 5;
+
+        int result = QuestionRemoveElementFromSortedArrays.removeDuplicates(nums);
+        assertEquals(expectedK, result);
+    }
+
+    @Test
+    public void testAllSame() {
+        int[] nums = {1,1,1,1};
+        int expectedInt = 1;
+
+        int result = QuestionRemoveElementFromSortedArrays.removeDuplicates(nums);
+        assertEquals(expectedInt, result);
+    }
+
+    @Test
+    public void testMixDuplicate() {
+        int[] nums = {1,1,2,2,3,3,4,4};
+        int expectedK = 4;
+
+        int result = QuestionRemoveElementFromSortedArrays.removeDuplicates(nums);
+        assertEquals(expectedK, result);
+    }
+
+    @Test
+    public void testSingleElement() {
+        int[] nums = {7};
+        int expectedK = 1;
 
         int result = QuestionRemoveElementFromSortedArrays.removeDuplicates(nums);
         assertEquals(expectedK, result);
