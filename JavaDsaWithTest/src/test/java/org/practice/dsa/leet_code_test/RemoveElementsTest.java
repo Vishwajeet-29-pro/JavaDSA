9dfbe27c945f0b24edf455becb7e0a2b94318144
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-07-23 08:57:24 +0530

    Add test cases for edge testing of removeElements() method.

--- JavaDsaWithTest/src/test/java/org/practice/dsa/leet_code_test/RemoveElementsTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/leet_code_test/RemoveElementsTest.java
@@ -16,4 +16,34 @@ public class RemoveElementsTest {
         int result = QuestionRemoveElements.removeElements(arr, val);
         assertEquals(expectedK, result);
     }
+
+    @Test
+    public void testRemoveElementsNoVal() {
+        int[] arr = {1,2,4,5};
+        int val = 3;
+        int expectedK = 4;
+
+        int result = QuestionRemoveElements.removeElements(arr, val);
+        assertEquals(expectedK, result);
+    }
+
+    @Test
+    public void testRemoveElementsAll() {
+        int[] arr = {2,2,2};
+        int val = 2;
+        int expectedK = 0;
+
+        int result = QuestionRemoveElements.removeElements(arr, val);
+        assertEquals(expectedK, result);
+    }
+
+    @Test
+    public void testRemoveElementsEmptyArray() {
+        int[] arr = {};
+        int val = 3;
+        int expectedK = 0;
+
+        int result = QuestionRemoveElements.removeElements(arr, val);
+        assertEquals(expectedK, result);
+    }
 }
