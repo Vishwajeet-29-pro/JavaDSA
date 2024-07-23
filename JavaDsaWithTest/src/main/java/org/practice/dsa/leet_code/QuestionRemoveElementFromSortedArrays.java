9df442d5c4b1c91d44addf7537de7cb8c46fe5a2
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-07-23 22:34:57 +0530

    Cover the edge case and pass the test case.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/leet_code/QuestionRemoveElementFromSortedArrays.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/leet_code/QuestionRemoveElementFromSortedArrays.java
@@ -16,6 +16,8 @@ public class QuestionRemoveElementFromSortedArrays {
     */
 
     public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
         int k = 0;
 
         for (int i = 1; i < nums.length; i++) {
