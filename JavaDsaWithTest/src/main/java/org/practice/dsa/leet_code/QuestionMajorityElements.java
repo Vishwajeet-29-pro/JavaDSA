4a73efb39efabdd80a213dacc08cde42e47f0d46
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-07-24 08:31:09 +0530

    Pass the test case and implement method majorityElements.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/leet_code/QuestionMajorityElements.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/leet_code/QuestionMajorityElements.java
@@ -1,4 +1,10 @@
 package org.practice.dsa.leet_code;
 
+import java.util.Arrays;
+
 public class QuestionMajorityElements {
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
 }
