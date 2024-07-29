aec95bd50cab0720b25db953576d61b1b033b720
Vishwajeet K <vishwajeet.k@vervetronics.com>
2024-07-29 18:29:35 +0530

    Add test case for validateNames and create method for validating names.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/code_kata/GildedRose.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/code_kata/GildedRose.java
@@ -9,10 +9,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!validateNames(i, "Aged Brie")
+                    && !validateNames(i,"Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!validateNames(i,"Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -20,7 +20,7 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (validateNames(i,"Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -36,15 +36,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!validateNames(i,"Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!validateNames(i,"Aged Brie")) {
+                    if (!validateNames(i,"Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!validateNames(i,"Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
@@ -58,5 +58,9 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    public boolean validateNames(int i, String itemName) {
+        return items[i].name.equals(itemName);
     }
 }
--- JavaDsaWithTest/src/test/java/org/practice/dsa/code_kata/GildedRoseTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/code_kata/GildedRoseTest.java
@@ -2,16 +2,28 @@ package org.practice.dsa.code_kata;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GildedRoseTest {
 
+    Item[] items = new Item[] { new Item("Aged Brie", 0, 0) };
+    GildedRose app = new GildedRose(items);
     @Test
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void validateItemsName() {
+        String itemName = "Aged Brie";
+
+        var result = app.validateNames(0, itemName);
+        assertTrue(result);
+        var result2 = app.validateNames(0, "foo");
+        assertFalse(result2);
     }
 
 }
