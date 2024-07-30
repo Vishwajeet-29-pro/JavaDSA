1c8e0ccaf528aa857ddf9b6607aaa8f2f1c21a52
Vishwajeet K <vishwajeet.k@vervetronics.com>
2024-07-30 10:42:29 +0530

    Add palindrome validation with test case using java 8.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/java8/functional_programming/Questions.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/functional_programming/Questions.java
@@ -1,6 +1,7 @@
 package org.practice.dsa.java8.functional_programming;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Questions {
     Function<Integer, Integer> fact = n-> {
@@ -8,7 +9,16 @@ public class Questions {
         return n * this.fact.apply(n-1);
     };
 
+    Predicate<String> isPalindrome = s -> {
+        String reverse = new StringBuilder(s).reverse().toString();
+        return s.equals(reverse);
+    };
+
     public int calculateFactorial(int number){
         return fact.apply(number);
+    }
+
+    public boolean validatePalindrome(String s) {
+        return isPalindrome.test(s);
     }
 }
--- JavaDsaWithTest/src/test/java/org/practice/dsa/java8/functional_programming/QuestionsTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/java8/functional_programming/QuestionsTest.java
@@ -2,7 +2,7 @@ package org.practice.dsa.java8.functional_programming;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class QuestionsTest {
     Questions questions = new Questions();
@@ -12,5 +12,14 @@ public class QuestionsTest {
         int result = questions.calculateFactorial(5);
 
         assertEquals(expected, result);
+    }
+
+    @Test
+    void testPalindrome() {
+        String input = "madam";
+
+        boolean result = questions.validatePalindrome(input);
+        assertTrue(result);
+        assertFalse(questions.validatePalindrome("today"));
     }
 }
