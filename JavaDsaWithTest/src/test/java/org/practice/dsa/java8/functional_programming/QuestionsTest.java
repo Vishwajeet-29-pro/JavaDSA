57489d977fb74acbc323045c7726ebbdb270fad5
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-07-31 08:25:28 +0530

    Add failing test case for merge lists.

--- JavaDsaWithTest/src/test/java/org/practice/dsa/java8/functional_programming/QuestionsTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/java8/functional_programming/QuestionsTest.java
@@ -2,6 +2,9 @@ package org.practice.dsa.java8.functional_programming;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class QuestionsTest {
@@ -21,5 +24,15 @@ public class QuestionsTest {
         boolean result = questions.validatePalindrome(input);
         assertTrue(result);
         assertFalse(questions.validatePalindrome("today"));
+    }
+
+    @Test
+    void testMergeLists() {
+        List<String> list1 = List.of("apple","mongo","kiwi");
+        List<String> list2 = List.of("banana","orange","pineapple");
+        List<String> expected = List.of("apple","banana","kiwi","mongo","orange","pineapple");
+
+        List<String> result = questions.mergeLists(list1, list2);
+        assertEquals(expected, result,"Result should be merged lists");
     }
 }
