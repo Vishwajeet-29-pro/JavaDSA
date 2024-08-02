99c890dcd5f3b1318b8c601c2afb0e7d2acd87bd
Vishwajeet K <vishwajeet.k@vervetronics.com>
2024-08-02 17:21:19 +0530

    Add test case for BackstagePass and Sulfuras class.

--- JavaDsaWithTest/src/test/java/org/practice/dsa/code_kata/factory_test/GildedRoseTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/code_kata/factory_test/GildedRoseTest.java
@@ -2,9 +2,7 @@ package org.practice.dsa.code_kata.factory_test;
 
 import org.junit.jupiter.api.Test;
 import org.practice.dsa.code_kata.Item;
-import org.practice.dsa.code_kata.factory_pattern.AbstractItems;
-import org.practice.dsa.code_kata.factory_pattern.AgedBrie;
-import org.practice.dsa.code_kata.factory_pattern.GildedRose;
+import org.practice.dsa.code_kata.factory_pattern.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,10 +11,28 @@ public class GildedRoseTest {
 
     @Test
     public void testCreateItem_AgedBrie() {
-        Item[] items = {new Item("AgedBrie",10,20)};
+        Item[] items = {new Item("Aged Brie",10,20)};
         gildedRose = new GildedRose(items);
 
         AbstractItems abstractItems = gildedRose.createItem(items[0]);
         assertInstanceOf(AgedBrie.class, abstractItems, "Expected an AgedBrie instance");
+    }
+
+    @Test
+    public void testCreateItem_BackStagePass() {
+        Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert", 10,20)};
+        gildedRose = new GildedRose(items);
+
+        AbstractItems abstractItems = gildedRose.createItem(items[0]);
+        assertInstanceOf(BackstagePass.class, abstractItems, "Expected an BackstagePass instance");
+    }
+
+    @Test
+    public void testCreateItem_Sulfuras() {
+        Item[] items = {new Item("Sulfuras, Hand of Ragnaros", 10,20)};
+        gildedRose = new GildedRose(items);
+
+        AbstractItems abstractItems = gildedRose.createItem(items[0]);
+        assertInstanceOf(Sulfuras.class, abstractItems,"Expected an Sulfuras instance");
     }
 }
