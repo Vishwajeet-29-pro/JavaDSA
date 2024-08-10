36b5089731df7087d94485e4d0a6bbc891a1f29e
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-10 17:48:52 +0530

    test: Add test case for the bubble sort algorithm.

--- JavaDsaWithTest/src/test/java/org/practice/dsa/algorithms/sorting/implementation/BubbleSortTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/algorithms/sorting/implementation/BubbleSortTest.java
@@ -1,4 +1,17 @@
 package org.practice.dsa.algorithms.sorting.implementation;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BubbleSortTest {
+    private final BubbleSort bubbleSort = new BubbleSort();
+
+    @Test
+    public void testBubbleSort() {
+        int[] arr = new int[] {10,8,6,4,2,0};
+        bubbleSort.bubbleSort(arr);
+        assertEquals(2, arr[1],"After swap the second number should be 2");
+        assertEquals(10, arr[arr.length-1], "After swap the last number should be 10");
+    }
 }
