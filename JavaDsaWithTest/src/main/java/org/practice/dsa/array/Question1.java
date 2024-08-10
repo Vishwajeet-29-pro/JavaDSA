343dba97fc49334dfb27a4dc9723c9a9845501af
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-10 18:30:35 +0530

    feat: Add methods for find second largest using bubble sort.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/array/Question1.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/array/Question1.java
@@ -28,4 +28,33 @@ public class Question1 {
         int[] sortedArray = Arrays.stream(arr).sorted().toArray();
         return sortedArray[sortedArray.length-2];
     }
+
+    public int secondLargestWithBubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) return Integer.MIN_VALUE;
+        bubbleSort(arr);
+        return arr[arr.length -2];
+    }
+
+    private void bubbleSort(int[] arr) {
+        boolean swapped;
+
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    private void swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
 }
