0855bcde7c7996a6cfa97c8a9912fe49269d5c12
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-11 17:35:32 +0530

    feat: Add implementation of linear search in method.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/algorithms/searching/implementation/LinearSearch.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/algorithms/searching/implementation/LinearSearch.java
@@ -1,2 +1,14 @@
-package org.practice.dsa.algorithms.searching.implementation;public class LinearSearch {
+package org.practice.dsa.algorithms.searching.implementation;
+
+public class LinearSearch {
+    public int linearSearch(int[] arr, int target) {
+        if (arr == null || arr.length < 1) return Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (target == arr[i]) return i;
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    /*Linear Search implementation*/
 }
