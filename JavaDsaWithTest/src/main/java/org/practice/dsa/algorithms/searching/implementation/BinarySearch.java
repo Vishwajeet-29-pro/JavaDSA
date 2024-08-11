c1f5388059f3c7ec1ed6cee0955f4a9ca68244df
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-11 18:16:21 +0530

    feat: Implement binary search algorithm.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/algorithms/searching/implementation/BinarySearch.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/algorithms/searching/implementation/BinarySearch.java
@@ -3,4 +3,22 @@ package org.practice.dsa.algorithms.searching.implementation;
 /* Binary Search algorithm implementation. */
 public class BinarySearch {
 
+    public int binarySearch(int[] arr, int target) {
+
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+
+            if (target == arr[mid]) {
+                return mid;
+            } else if (target < arr[mid]) {
+                end = mid -1;
+            } else {
+                start = mid +1;
+            }
+        }
+        return -1;
+    }
 }
