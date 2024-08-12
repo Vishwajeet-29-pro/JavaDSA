438c4874caf96bbeb1d9c1c9afe06980051017bb
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-12 21:10:25 +0530

    feat: Implement condition for empty array and pass test case.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/algorithms/sorting/implementation/CyclicSort.java
@@ -0,0 +1,9 @@
+package org.practice.dsa.algorithms.sorting.implementation;
+
+public class CyclicSort {
+    /*Cyclic sort implementation*/
+    public void cyclicSort(int[] arr) {
+        if (arr == null || arr.length < 1) throw new RuntimeException("Empty array passed.");
+
+    }
+}
