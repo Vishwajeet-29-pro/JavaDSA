eb07038d775f20fac7d6770f0991c9440fbe204d
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-12 21:21:24 +0530

    feat: Fully implemented cyclic sort algorithm.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/algorithms/sorting/implementation/CyclicSort.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/algorithms/sorting/implementation/CyclicSort.java
@@ -5,5 +5,20 @@ public class CyclicSort {
     public void cyclicSort(int[] arr) {
         if (arr == null || arr.length < 1) throw new RuntimeException("Empty array passed.");
 
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private void swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 }
