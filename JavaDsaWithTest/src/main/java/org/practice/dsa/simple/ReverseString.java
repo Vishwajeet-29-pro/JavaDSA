ee8e97815be3b2e6cd078a0fef719bf27f62cff4
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-17 07:17:53 +0530

    feat: Add method reverseString and handle empty input.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/simple/ReverseString.java
@@ -0,0 +1,9 @@
+package org.practice.dsa.simple;
+
+public class ReverseString {
+    public String reverseString(String input) {
+        if (input.isEmpty()) throw new RuntimeException("Empty string passed");
+
+        return null;
+    }
+}
