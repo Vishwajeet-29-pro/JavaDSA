6a6b5c7c95a0c4912e4b5729cc0977262424f62e
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-17 07:15:19 +0530

    test: Add test case for exception handling when input is empty.

---
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/simple/ReverseStringTest.java
@@ -0,0 +1,18 @@
+package org.practice.dsa.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseStringTest {
+    private final ReverseString rs = new ReverseString();
+
+    @Test
+    public void testReverseStringIfEmptyInput() {
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            rs.reverseString("");
+        });
+        assertEquals("Empty string passed", exception.getMessage());
+    }
+
+}
