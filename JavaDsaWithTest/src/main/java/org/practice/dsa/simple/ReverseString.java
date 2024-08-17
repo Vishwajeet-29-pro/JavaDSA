4d0aaa9468560a3fcbeef9a4fc767e2e570964c9
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-17 07:24:20 +0530

    feat: Implement the reverse String logic.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/simple/ReverseString.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/simple/ReverseString.java
@@ -4,6 +4,12 @@ public class ReverseString {
     public String reverseString(String input) {
         if (input.isEmpty()) throw new RuntimeException("Empty string passed");
 
-        return null;
+        StringBuilder builder = new StringBuilder();
+        char[] chars = input.toCharArray();
+
+        for(int i = chars.length -1; i >=0; i-- )   {
+            builder.append(chars[i]);
+        }
+        return builder.toString();
     }
 }
