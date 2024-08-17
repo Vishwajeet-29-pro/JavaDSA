098c5d165969d028e8e071cb74ae0233f748eb3c
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-17 17:39:15 +0530

    feat: Use some classes and methods from DateTimeApi.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/java8/datetime/DateTimeApi.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/datetime/DateTimeApi.java
@@ -1,6 +1,19 @@
 package org.practice.dsa.java8.datetime;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class DateTimeApi {
 
+    public static void localDateTimeApi() {
+        LocalDate date = LocalDate.now();
+        System.out.println("The current date is: "+date);
 
+        LocalTime time = LocalTime.now();
+        System.out.println("The current time is: "+time);
+    }
+
+    public static void main(String[] args) {
+        localDateTimeApi();
+    }
 }
