72bf6defb1b4e5b5ed179c5c82e5ca23e31a6958
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-17 16:55:12 +0530

    refactor: Move DateTimeApi class to datetime package.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/datetime/DateTimeApi.java
@@ -0,0 +1,6 @@
+package org.practice.dsa.java8.datetime;
+
+public class DateTimeApi {
+
+
+}
--- JavaDsaWithTest/src/main/java/org/practice/dsa/java8/lamdba_expressions/DateTimeApi.java
+++
@@ -1,6 +0,0 @@
-package org.practice.dsa.java8.lamdba_expressions;
-
-public class DateTimeApi {
-
-
-}
