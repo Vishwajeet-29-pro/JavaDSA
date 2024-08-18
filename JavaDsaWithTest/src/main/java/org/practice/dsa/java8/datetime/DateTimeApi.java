30bce01e2ab6be47263d0b81cdd218e96027617d
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-18 10:44:32 +0530

    feat: Implement methods from various classes of DateTimeApi

--- JavaDsaWithTest/src/main/java/org/practice/dsa/java8/datetime/DateTimeApi.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/datetime/DateTimeApi.java
@@ -1,7 +1,10 @@
 package org.practice.dsa.java8.datetime;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 
 public class DateTimeApi {
 
@@ -11,6 +14,26 @@ public class DateTimeApi {
 
         LocalTime time = LocalTime.now();
         System.out.println("The current time is: "+time);
+
+        LocalDateTime current = LocalDateTime.now();
+        System.out.println("Current date and time is "+current);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-YYYY HH:mm:SS");
+        String formatedDateTime = current.format(formatter);
+
+        System.out.println("In formatted manner "+formatedDateTime);
+
+        Month month = current.getMonth();
+        int day = current.getDayOfMonth();
+        int year = current.getYear();
+
+        System.out.println("Month: "+month+" Day: "+day+" Year: "+year);
+
+        LocalDate date1 = LocalDate.of(1950, 1, 26);
+        System.out.println("The First Republic Day : "+date1);
+
+        LocalDateTime specificDate = current.withDayOfMonth(24).withYear(2016);
+        System.out.println("Specific date with current time: "+specificDate);
     }
 
     public static void main(String[] args) {
