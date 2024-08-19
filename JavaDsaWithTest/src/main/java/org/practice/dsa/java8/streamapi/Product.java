66311751a5f828f44924d954d60ac4f42d84a708
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-19 14:46:06 +0530

    feat: Add Product class for Stream api implementation.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/streamapi/Product.java
@@ -0,0 +1,34 @@
+package org.practice.dsa.java8.streamapi;
+
+public class Product {
+    private String name;
+    private double price;
+    private String category;
+
+    public Product(String name, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
