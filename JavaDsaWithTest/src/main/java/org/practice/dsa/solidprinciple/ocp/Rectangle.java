8a8cebd724ff2d064c8185d81d7fb6710a1baf2f
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-08-20 10:37:12 +0530

    refactor: Add Rectangle class and implements ShapeInterface and override method.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/solidprinciple/ocp/Rectangle.java
@@ -0,0 +1,16 @@
+package org.practice.dsa.solidprinciple.ocp;
+
+public class Rectangle implements ShapeInterface {
+    private final double length;
+    private final double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
