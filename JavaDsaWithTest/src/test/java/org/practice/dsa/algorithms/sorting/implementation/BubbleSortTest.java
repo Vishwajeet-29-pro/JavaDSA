0a64555c48f40230c8b08348b9e8a7c58748ef74
Vishwajeet K <vishwajeet.k@vervetronics.com>
2024-08-27 15:39:40 +0530

    test: Add test case for Bubble sort with correct assertion method from JUnit5.

--- JavaDsaWithTest/src/test/java/org/practice/dsa/algorithms/sorting/implementation/BubbleSortTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/algorithms/sorting/implementation/BubbleSortTest.java
@@ -2,18 +2,25 @@ package org.practice.dsa.algorithms.sorting.implementation;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BubbleSortTest {
-    private final BubbleSort bubbleSort = new BubbleSort();
+    private final BubbleSort sort = new BubbleSort();
 
     @Test
     public void testBubbleSort() {
         int[] arr = new int[] {10,8,6,4,2,0};
-        bubbleSort.bubbleSort(arr);
+        sort.bubbleSort(arr);
         assertEquals(2, arr[1],"After swap the second number should be 2");
         assertEquals(10, arr[arr.length-1], "After swap the last number should be 10");
+    }
+
+    @Test
+    public void testBubbleSortWithCorrectAssertionMethod() {
+        int[] arr = {10,8,6,4,2,0};
+        int[] expected = {0,2,4,6,8,10};
+        sort.bubbleSort(arr);
+        assertArrayEquals(expected, arr);
     }
 
     @Test
@@ -21,7 +28,7 @@ public class BubbleSortTest {
         int[] arr = {};
 
         Exception exception = assertThrows(RuntimeException.class,() -> {
-            bubbleSort.bubbleSort(arr);
+            sort.bubbleSort(arr);
         });
 
         assertEquals("Empty array passed", exception.getMessage());
