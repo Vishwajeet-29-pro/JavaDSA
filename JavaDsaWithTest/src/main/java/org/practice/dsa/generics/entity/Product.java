e0545e5394b9572cc1446788abcf56f3dec038fe
Vishwajeet K <vishwajeet.k@vervetronics.com>
2024-08-28 14:38:04 +0530

    feat: Add Product entity for generics implementation.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/generics/entity/Product.java
@@ -0,0 +1,27 @@
+package org.practice.dsa.generics.entity;
+
+public class Product {
+    private final int productId;
+    private final String productName;
+
+    public Product(int productId, String productName) {
+        this.productId = productId;
+        this.productName = productName;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                '}';
+    }
+}
