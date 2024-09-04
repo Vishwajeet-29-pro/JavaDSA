236838c74c10546ed90b5dddead52f0b404fce67
Vishwajeet K <vishwajeet.k@vervetronics.com>
2024-09-04 17:05:49 +0530

    feat: Add method for find book by title and make test case pass.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/java8/exercise/library/Library.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/exercise/library/Library.java
@@ -2,6 +2,7 @@ package org.practice.dsa.java8.exercise.library;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Library {
     List<Book> books;
@@ -12,5 +13,12 @@ public class Library {
 
     public void addBook(Book book) {
         books.add(book);
+    }
+
+    // filter books by title
+    public Optional<Book> findBookByTitle(String title) {
+        return books.stream()
+                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .findFirst();
     }
 }
