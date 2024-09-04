9e7adeaba85177e76d2578b11e6bbdde757ffd13
Vishwajeet K <vishwajeet.k@vervetronics.com>
2024-09-04 12:45:02 +0530

    feat: Add Tiger subclass to Animal.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/oops/abstraction/animal/Tiger.java
@@ -0,0 +1,8 @@
+package org.practice.dsa.oops.abstraction.animal;
+
+public class Tiger extends Animal {
+    @Override
+    void sound() {
+        System.out.println("The Tiger making sound....!!");
+    }
+}
