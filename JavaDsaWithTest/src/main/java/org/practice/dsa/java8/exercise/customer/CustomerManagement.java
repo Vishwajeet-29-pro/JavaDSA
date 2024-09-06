2ca424e78a5b6fcc90f498aab4d20b752888675e
Vishwajeet K <vishwajeet.k@vervetronics.com>
2024-09-06 15:06:37 +0530

    feat: Implement method for most expensive item and make test pass.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/java8/exercise/customer/CustomerManagement.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/exercise/customer/CustomerManagement.java
@@ -1,5 +1,8 @@
 package org.practice.dsa.java8.exercise.customer;
 
+import java.util.Comparator;
+import java.util.Optional;
+
 public class CustomerManagement {
 
     // Get the total value of all orders for a given customer.
@@ -9,4 +12,13 @@ public class CustomerManagement {
                 .mapToDouble(Item::getPrice)
                 .sum();
     }
+
+    // Find the most expensive item across all orders for a customer.
+    public Optional<Item> findExpensiveItem(Customer customer) {
+        return customer.getOrders().stream()
+                .flatMap(order -> order.getItems().stream())
+                .max(Comparator.comparingDouble(Item::getPrice));
+
+    }
+
 }
