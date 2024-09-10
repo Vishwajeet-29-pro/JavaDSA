e4b7d9a1641d0c3dc2bd0231832858fe071a197c
Vishwajeet K <vishwajeet.k@vervetronics.com>
2024-09-10 17:13:43 +0530

    feat: Implement method wih filter and flatmap of java 8 and make test pass.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/exercise/flight/FlightManagement.java
@@ -0,0 +1,14 @@
+package org.practice.dsa.java8.exercise.flight;
+
+import java.util.List;
+
+public class FlightManagement {
+
+    // Task 1: Find all passengers flying to a specific destination
+    public List<Passenger> findAllPassengersByDestination(List<Flight> flights, String destination) {
+        return flights.stream()
+                .filter(flight -> flight.getDestination().equalsIgnoreCase(destination))
+                .flatMap(flight -> flight.getPassengers().stream())
+                .toList();
+    }
+}
