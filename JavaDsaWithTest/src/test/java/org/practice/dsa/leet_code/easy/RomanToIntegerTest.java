c4c06aa6d3bd003564e9241317c04e474672db9b
Vishwajeet K <vishwajeet.k@vervetronics.com>
2024-09-20 16:47:03 +0530

    test: Add tests for Roman to integer conversion.

--- JavaDsaWithTest/src/test/java/org/practice/dsa/leet_code/easy/RomanToIntegerTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/leet_code/easy/RomanToIntegerTest.java
@@ -11,6 +11,41 @@ public class RomanToIntegerTest {
     public void testRomanToInteger() {
         String s = "III";
         int expected = 3;
-        assertEquals(expected, roman.romanToInt(s));
+        assertEquals(expected, roman.optimizedRomanToInt(s));
+    }
+
+    @Test
+    public void testRomanNumberGreaterThanFirstOne() {
+        String s = "CCI";
+        int expected = 201;
+        assertEquals(expected, roman.optimizedRomanToInt(s));
+    }
+
+    @Test
+    public void testSubtractMethod() {
+        int expected = 9;
+        assertEquals(expected, roman.optimizedRomanToInt("IX"));
+    }
+
+    @Test
+    public void testIfStringContainOnlyOneRomanNumber() {
+        int expected = 100;
+        assertEquals(expected, roman.optimizedRomanToInt("C"));
+    }
+
+    @Test
+    public void testBiggerNumber() {
+        String s = "MCMXCIV";
+        int expected = 1994;
+
+        assertEquals(expected, roman.optimizedRomanToInt(s));
+    }
+
+    @Test
+    public void testRomanNumberLVIII() {
+        String s = "LVIII";
+        int expected = 58;
+
+        assertEquals(expected, roman.optimizedRomanToInt(s));
     }
 }
