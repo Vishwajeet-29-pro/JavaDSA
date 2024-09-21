c95da6dbebc9d3e6326bf5f38eccfbb5e09e8106
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-09-21 15:41:13 +0530

    test: Add some other test cases for validating parenthesis

--- JavaDsaWithTest/src/test/java/org/practice/dsa/leet_code/easy/ValidParenthesisTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/leet_code/easy/ValidParenthesisTest.java
@@ -2,7 +2,7 @@ package org.practice.dsa.leet_code.easy;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidParenthesisTest {
     private final QuestionValidParenthesis parenthesis = new QuestionValidParenthesis();
@@ -11,5 +11,30 @@ public class ValidParenthesisTest {
     public void testValidParenthesis() {
         String s = "()";
         assertTrue(parenthesis.isValid(s));
+    }
+
+    @Test
+    public void testValidParenthesisForMultipleBrackets() {
+        String s = "(){}[]";
+        assertTrue(parenthesis.isValid(s));
+    }
+
+    @Test
+    public void testValidParenthesisForWrongMatch() {
+        String s = "(}";
+        assertFalse(parenthesis.isValid(s));
+    }
+
+    @Test
+    public void testMultipleAssertions() {
+       assertAll("Valid Parenthesis",() -> {
+           assertTrue(parenthesis.isValid("([])"));
+           assertFalse(parenthesis.isValid("(}[){]"));
+       });
+    }
+
+    @Test
+    public void testIncompleteParenthesis() {
+        assertFalse(parenthesis.isValid("(){}}{"));
     }
 }
