2e8aa689cf1cd0fc5f87083349634e81b9aad7b5
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-09-22 16:46:00 +0530

    tests: Make two tests pass.

--- JavaDsaWithTest/src/test/java/org/practice/dsa/leet_code/easy/SearchInsertPositionTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/leet_code/easy/SearchInsertPositionTest.java
@@ -7,13 +7,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SearchInsertPositionTest {
     private final QuestionSearchInsertPosition insertPosition = new QuestionSearchInsertPosition();
 
+    int[] arr = {1,3,5,7};
+
     @Test
     public void testSearchInsert() {
-        int[] arr = {1,3,5,7};
+
         int target = 5;
         int expected = 2;
         int actual = insertPosition.searchInsert(arr, target);
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testSearchInsertForSmallerNumber() {
+        int target = 2;
+        int expected = 1;
+        int actual = insertPosition.searchInsert(arr, target);
+        assertEquals(expected, actual);
+    }
 }
