c6c283467a504b27141b6cd60981925381c1c7c9
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-09-22 16:45:26 +0530

    feat: Implement binary search algorithm for solving problem.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/leet_code/easy/QuestionSearchInsertPosition.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/leet_code/easy/QuestionSearchInsertPosition.java
@@ -4,6 +4,21 @@ public class QuestionSearchInsertPosition {
 
     public int searchInsert(int[] nums, int target) {
 
-        return 0;
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return start;
     }
 }
