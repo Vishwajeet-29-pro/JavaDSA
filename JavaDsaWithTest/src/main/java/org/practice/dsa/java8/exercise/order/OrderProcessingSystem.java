25ed776c4b7693c986a449ef2d43cdfa3dc74e16
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-09-24 16:46:25 +0530

    feat: Add method to find most expensive product.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/java8/exercise/order/OrderProcessingSystem.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/exercise/order/OrderProcessingSystem.java
@@ -2,6 +2,7 @@ package org.practice.dsa.java8.exercise.order;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class OrderProcessingSystem {
 
@@ -12,5 +13,12 @@ public class OrderProcessingSystem {
                 .flatMap(product -> product.getProducts().stream())
                 .mapToDouble(Product::getPrice)
                 .sum();
+    }
+
+    // Find the most expensive product across all orders.
+    public Optional<Product> findMostExpensiveProduct(List<Order> orders) {
+        return orders.stream()
+                .flatMap(order -> order.getProducts().stream())
+                .max(Comparator.comparingDouble(Product::getPrice));
     }
 }
