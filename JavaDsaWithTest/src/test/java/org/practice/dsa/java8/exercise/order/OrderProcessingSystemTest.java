dd979f2f57deedbd9a55e2badbf19f67cc8d9d56
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-09-24 16:17:52 +0530

    test: Add setup for tests and create failing test for find total price of complete orders.

---
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/java8/exercise/order/OrderProcessingSystemTest.java
@@ -0,0 +1,39 @@
+package org.practice.dsa.java8.exercise.order;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderProcessingSystemTest {
+
+    private OrderProcessingSystem orderProcessing;
+    private List<Order> orders;
+
+    @BeforeEach
+    public void setup() {
+        Product p1 = new Product("CPU", 25000);
+        Product p2 = new Product("Monitor", 20000);
+        Product p3 = new Product("Mouse", 1000);
+        Product p4 = new Product("Keyboard", 1400);
+
+        Order o1 = new Order("COMPLETED", List.of(p1, p2, p3));
+        Order o2 = new Order("PENDING", List.of(p1, p2, p4));
+        Order o3 = new Order("COMPLETED", List.of(p1, p2, p3, p4));
+        Order o4 = new Order("CANCELED", List.of(p3, p4));
+
+        orders = List.of(o1, o2, o3, o4);
+    }
+
+    // Find the total price of all completed orders.
+    @Test
+    public void testFindTotalPriceOfAllCompleteOrders() {
+        int expected = 93400;
+        int actual = orderProcessing.findTotalPriceOfAllCompleteOrder(orders);
+        assertEquals(expected, actual);
+    }
+}
