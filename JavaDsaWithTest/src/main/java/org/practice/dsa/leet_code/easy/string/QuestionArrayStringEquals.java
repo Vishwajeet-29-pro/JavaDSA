70b9832247b818b13f09e3f3b0f2ced846ef3970
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-10-05 16:19:10 +0530

    test: Add failing test for ArrayStringEquals.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/leet_code/easy/string/QuestionArrayStringEquals.java
@@ -0,0 +1,9 @@
+package org.practice.dsa.leet_code.easy.string;
+
+public class QuestionArrayStringEquals {
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+
+        return false;
+    }
+}
---
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/leet_code/easy/string/ArrayStringEquals.java
@@ -0,0 +1,18 @@
+package org.practice.dsa.leet_code.easy.string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ArrayStringEquals {
+
+    private final QuestionArrayStringEquals stringEquals = new QuestionArrayStringEquals();
+
+    @Test
+    public void testArrayStringsEquals() {
+        String[] input1 = {"ab", "c"};
+        String[] input2 = {"a", "bc"};
+        boolean result = stringEquals.arrayStringsAreEqual(input1, input2);
+        assertTrue(result);
+    }
+}
