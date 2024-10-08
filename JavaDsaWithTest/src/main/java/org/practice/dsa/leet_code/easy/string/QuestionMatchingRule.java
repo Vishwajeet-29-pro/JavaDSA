ccd740345d888b3fc1130f429cdfda52e7158d0c
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-10-08 17:35:59 +0530

    test: Add failing test for matching rule.

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/leet_code/easy/string/QuestionMatchingRule.java
@@ -0,0 +1,12 @@
+package org.practice.dsa.leet_code.easy.string;
+
+import java.util.List;
+
+public class QuestionMatchingRule {
+
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+
+
+        return 0;
+    }
+}
