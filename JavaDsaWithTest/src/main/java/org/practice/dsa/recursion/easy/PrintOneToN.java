d1c1a7ce2485f2e259f7ccb40653d32c54c6f250
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-10-10 17:49:01 +0530

    refactor: Remove main method and make print1ToN method non-static.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/easy/PrintOneToN.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/easy/PrintOneToN.java
@@ -2,15 +2,11 @@ package org.practice.dsa.recursion.easy;
 
 public class PrintOneToN {
 
-    public static void print1ToN(int n) {
+    public void print1ToN(int n) {
         if (n == 0) {
             return;
         }
         System.out.println(n);
         print1ToN(n-1);
-    }
-
-    public static void main(String[] args) {
-        print1ToN(5);
     }
 }
