1c54c0240042127982a25cf1d3e75a74b25b51dc
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-10-15 10:43:54 +0530

    test: Add another test to verify when target not found.

--- JavaDsaWithTest/src/test/java/org/practice/dsa/recursion/array/LinearSearchUsingRecursionTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/recursion/array/LinearSearchUsingRecursionTest.java
@@ -8,11 +8,20 @@ class LinearSearchUsingRecursionTest {
 
     private final LinearSearchUsingRecursion search = new LinearSearchUsingRecursion();
 
+    // test creation steps went wrong, i.e. false condition should need to handle first.
     @Test
     public void test_when_target_found_should_return_true() {
         int[] arr = {1, 6, 3, 0, 7, 4};
         int target = 6;
 
         assertTrue(search.findElement(arr, target, 0));
+    }
+
+    @Test
+    public void test_when_target_not_found_should_return_false() {
+        int[] arr = {1,5,3,2,9};
+        int target = 8;
+
+        assertFalse(search.findElement(arr, target, 0));
     }
 }
