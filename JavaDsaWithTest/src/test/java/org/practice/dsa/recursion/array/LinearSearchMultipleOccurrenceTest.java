6aeaf06804c346047b524538fc8d8d4664bff11d
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-10-16 10:54:02 +0530

    feat: Add another and optimize approach to get the list of elements.

--- JavaDsaWithTest/src/test/java/org/practice/dsa/recursion/array/LinearSearchMultipleOccurrenceTest.java
+++ JavaDsaWithTest/src/test/java/org/practice/dsa/recursion/array/LinearSearchMultipleOccurrenceTest.java
@@ -2,6 +2,7 @@ package org.practice.dsa.recursion.array;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,5 +18,14 @@ class LinearSearchMultipleOccurrenceTest {
         List<Integer> actual = multi.linearSearchMultipleOccurrence(arr, 3, 0);
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMultipleOccurrenceByPassingListInFunctionCall() {
+        int[] arr = {1, 3, 5, 2, 8, 0, 0};
+        int target = 0;
+        List<Integer> actual = multi.linearSearchMultiOccurrence(arr, target, 0, new ArrayList<>());
+
+        assertEquals(List.of(5, 6), actual);
     }
 }
