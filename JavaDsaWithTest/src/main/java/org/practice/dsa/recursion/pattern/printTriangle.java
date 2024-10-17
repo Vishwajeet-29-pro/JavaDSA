2f3bd4dbbf6c8271ab3ba8e0080253e13473396a
VishwajeetVT <vishwajeet.k@vervetronics.com>
2024-10-17 10:20:49 +0530

    feat: Add another type of triangle pattern using same recursion function, just change in condition.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/pattern/printTriangle.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/pattern/printTriangle.java
@@ -3,6 +3,8 @@ package org.practice.dsa.recursion.pattern;
 public class printTriangle {
     public static void main(String[] args) {
         triangle(4, 0);
+        System.out.println();
+        printTriangle2(4, 0);
     }
     public static void triangle(int r, int c) {
         if (r == 0){
@@ -14,6 +16,19 @@ public class printTriangle {
         } else {
             System.out.println();
             triangle(r-1, 0);
+        }
+    }
+
+    public static void printTriangle2(int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            printTriangle2(r, c+1);
+            System.out.print("*");
+        } else {
+            printTriangle2(r-1, 0);
+            System.out.println();
         }
     }
 }
