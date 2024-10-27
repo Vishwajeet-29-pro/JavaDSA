480693953ca8e278eff434b5d8ffae5a084ddae3
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-10-27 14:20:19 +0530

    feat: Add Permutations method return list using recursion.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/subset/Permutations.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/subset/Permutations.java
@@ -1,8 +1,12 @@
 package org.practice.dsa.recursion.subset;
 
+import java.util.ArrayList;
+
 public class Permutations {
     public static void main(String[] args) {
-        permutations("", "abc");
+//        permutations("", "abc");
+       var result= permutationsList("", "abc");
+        System.out.println(result);
     }
 
     static void permutations(String p, String up) {
@@ -17,5 +21,22 @@ public class Permutations {
             String s = p.substring(i, p.length());
             permutations(f+ch+s, up.substring(1));
         }
+    }
+
+    static ArrayList<String> permutationsList(String p, String up) {
+        if (up.isEmpty()) {
+           ArrayList<String> list = new ArrayList<>();
+           list.add(p);
+           return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> ans = new ArrayList<>();
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0,i);
+            String s = p.substring(i, p.length());
+            ans.addAll(permutationsList(f+ch+s, up.substring(1)));
+        }
+        return ans;
     }
 }
