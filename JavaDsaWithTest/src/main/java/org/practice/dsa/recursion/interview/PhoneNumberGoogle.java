690b6ee997734be1c0b62ebbd340af970566ccc0
VishwajeetK <vishwajeet.k@vervetronics.com>
2024-10-28 18:26:55 +0530

    feat: Add phoneNumber question return list.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/interview/PhoneNumberGoogle.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/interview/PhoneNumberGoogle.java
@@ -1,13 +1,17 @@
 package org.practice.dsa.recursion.interview;
 
+import java.util.ArrayList;
+
 public class PhoneNumberGoogle {
     // This question asked in the Google interview
     // let's start with basic implementation and then will update code according to leet code
 
     public static void main(String[] args) {
 //        phoneNumber("", "12");
-        phoneNumber("","1");
+//        phoneNumber("","1");
+        System.out.println(phoneNumberList("", "1"));
     }
+
     // p = processed, up = unprocessed
     public static void phoneNumber(String p, String up) {
         if (up.isEmpty()) {
@@ -22,5 +26,21 @@ public class PhoneNumberGoogle {
             char ch = (char) ('a' + i);
             phoneNumber(p + ch, up.substring(1));
         }
+    }
+
+    public static ArrayList<String> phoneNumberList(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        int digit = up.charAt(0) - '0';
+        int endIndex = (digit == 9) ? 26 : digit * 3;
+        for (int i = (digit -1) *3; i < endIndex; i++) {
+            char ch = (char) ('a' + i);
+            ans.addAll(phoneNumberList(p + ch, up.substring(1)));
+        }
+        return ans;
     }
 }
