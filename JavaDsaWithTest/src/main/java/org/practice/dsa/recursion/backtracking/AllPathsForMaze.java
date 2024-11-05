8beb98efe6d66988166aa6a6d805fc7d91f86b09
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-11-05 08:26:16 +0530

    feat: Add method to print the path and step matrix.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/backtracking/AllPathsForMaze.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/backtracking/AllPathsForMaze.java
@@ -1,5 +1,7 @@
 package org.practice.dsa.recursion.backtracking;
 
+import java.util.Arrays;
+
 public class AllPathsForMaze {
     public static void main(String[] args) {
         boolean[][] board = {
@@ -8,7 +10,10 @@ public class AllPathsForMaze {
                 {true, true, true}
         };
 
-        allPaths("", board, 0, 0);
+//        allPaths("", board, 0, 0);
+
+        int[][] arr = new int[board.length][board[0].length];
+        allPathsPrint("", board, 0, 0, arr, 1);
     }
 
     static void allPaths(String p, boolean[][] maze, int r, int c) {
@@ -40,5 +45,43 @@ public class AllPathsForMaze {
        }
 
        maze[r][c] = true;
+    }
+
+    static void allPathsPrint(String p, boolean[][] maze, int r, int c, int[][] path, int step) {
+        if (r == maze.length -1 && c == maze[0].length -1) {
+            path[r][c] = step;
+            for (int[] arr: path) {
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+
+        if (!maze[r][c]) {
+            return;
+        }
+
+        maze[r][c] = false;
+        path[r][c] = step;
+
+        if (r < maze.length -1) {
+            allPathsPrint(p + "D", maze, r+1, c, path, step+1);
+        }
+
+        if (c < maze[0].length - 1) {
+            allPathsPrint(p + "R", maze, r, c+1, path, step+1);
+        }
+
+        if (r > 0) {
+            allPathsPrint(p + "U", maze, r-1, c, path, step+1);
+        }
+
+        if (c > 0) {
+            allPathsPrint(p + "L", maze, r, c-1, path, step+1);
+        }
+
+        maze[r][c] = true;
+        path[r][c] = 0;
     }
 }
