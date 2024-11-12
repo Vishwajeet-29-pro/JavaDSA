bd72a4529e56cdf83bb03ab7b11218979caba24f
VishwajeetK <vishwajeet.k@vervetronics.com>
2024-11-12 12:41:05 +0530

    feat: Add method to validate the given integer is power of three.

--- JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/easy/Power.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/recursion/easy/Power.java
@@ -3,6 +3,7 @@ package org.practice.dsa.recursion.easy;
 public class Power {
     public static void main(String[] args) {
         System.out.println(isPowerOfTwo(3));
+        System.out.println(isPowerOfThree(9));
     }
 
     public static boolean isPowerOfTwo(int n) {
@@ -14,5 +15,16 @@ public class Power {
             return false;
         }
         return isPowerOfTwo(n/2);
+    }
+
+    public static boolean isPowerOfThree(int n) {
+        if (n == 1) {
+            return true;
+        }
+
+        if (n <= 0 || n % 3 != 0) {
+            return false;
+        }
+        return isPowerOfThree(n/3);
     }
 }
