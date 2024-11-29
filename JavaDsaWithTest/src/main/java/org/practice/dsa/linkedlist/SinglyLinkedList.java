f220201a610716e0f6c3dad61407fe5320cc3c11
VishwajeetK <vishwajeet.k@vervetronics.com>
2024-11-29 11:00:38 +0530

    feat: Add find method

--- JavaDsaWithTest/src/main/java/org/practice/dsa/linkedlist/SinglyLinkedList.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/linkedlist/SinglyLinkedList.java
@@ -91,6 +91,17 @@ public class SinglyLinkedList {
         return val;
     }
 
+    public Node find(int val) {
+        Node node = head;
+        while (node != null) {
+            if (node.val == val) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
     private Node get(int index) {
         Node temp = head;
         for (int i = 0; i < index; i++) {
