19d0c573eb4e335547d365b6f69a1a53f0564b8e
VishwajeetK <vishwajeet.k@vervetronics.com>
2024-12-04 14:53:19 +0530

    feat: Add filter code to extract elements from list starts with given letter (java 8)

--- JavaDsaWithTest/src/main/java/org/practice/dsa/java8/functional_programming/filter/FilterInDeep.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/functional_programming/filter/FilterInDeep.java
@@ -6,11 +6,21 @@ public class FilterInDeep {
     public static void main(String[] args) {
         List<Integer> integerList = List.of(1,2,3,4,5,6);
         System.out.println(evenNumbers(integerList));
+
+        List<String> stringList = List.of("Java", "JavaScript", "Kotlin", "Python");
+        System.out.println(specificLetterList(stringList, "J"));
     }
 
     public static List<Integer> evenNumbers(List<Integer> list) {
         return list.stream()
                 .filter(f -> f % 2 == 0)
+                .toList();
+    }
+
+    // Filter List of string and return list starts with specific letter
+    public static List<String> specificLetterList(List<String> list, String ch) {
+        return list.stream()
+                .filter(s -> s.startsWith(ch))
                 .toList();
     }
 }
