844b949f71863ace08dd98aca0ea5738813d092a
VishwajeetK <vishwajeet.k@vervetronics.com>
2024-12-04 11:42:41 +0530

    feat: Add example on filter method (java 8)

---
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/java8/functional_programming/filter/FilterInDeep.java
@@ -0,0 +1,16 @@
+package org.practice.dsa.java8.functional_programming.filter;
+
+import java.util.List;
+
+public class FilterInDeep {
+    public static void main(String[] args) {
+        List<Integer> integerList = List.of(1,2,3,4,5,6);
+        System.out.println(evenNumbers(integerList));
+    }
+
+    public static List<Integer> evenNumbers(List<Integer> list) {
+        return list.stream()
+                .filter(f -> f % 2 == 0)
+                .toList();
+    }
+}
