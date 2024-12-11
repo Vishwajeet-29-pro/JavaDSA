843df821c55b43adcd8a3ba44f733bb5d0c63dba
VishwajeetK <vishwajeet.k@vervetronics.com>
2024-12-11 14:18:01 +0530

    feat: Add another method to merge two sorted arrays

--- JavaDsaWithTest/src/main/java/org/practice/dsa/leet_code/easy/sorting/InsertionOfArrays.java
+++ JavaDsaWithTest/src/main/java/org/practice/dsa/leet_code/easy/sorting/InsertionOfArrays.java
@@ -9,7 +9,8 @@ public class InsertionOfArrays {
         int[] arr1 = {1,2,2,1};
         int[] arr2 = {2,2};
 
-        System.out.println(Arrays.toString(insertion(arr1, arr2)));
+//        System.out.println(Arrays.toString(insertion(arr1, arr2)));
+        System.out.println(Arrays.toString(merge(arr1, arr2)));
     }
 
     public static int[] insertion(int[] nums1, int[] nums2) {
@@ -34,5 +35,37 @@ public class InsertionOfArrays {
             }
         }
         return list.stream().mapToInt(k ->k).toArray();
+    }
+
+    public static int[] merge(int[] num1, int[] num2) {
+        Arrays.sort(num1);
+        Arrays.sort(num2);
+        int[] mix = new int[num1.length + num2.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < num1.length && j < num2.length) {
+            if (num1[i] < num2[j]) {
+                mix[k] = num1[i];
+                i++;
+            } else {
+                mix[k] = num2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < num1.length) {
+            mix[k] = num1[i];
+            i++;
+            k++;
+        }
+
+        while (j < num2.length) {
+            mix[k] = num2[j];
+            j++;
+            k++;
+        }
+        return mix;
     }
 }
